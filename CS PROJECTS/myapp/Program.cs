1831037ede11a6ca4a0c901c0228ff02956d89d4
Manoj-rango <manojk.yenugula@gmail.com>
2020-11-17 15:08:26 +0530

    my app

---
+++ CS PROJECTS/myapp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace myapp
+{
+    class Program
+    {
+        static void manoh()
+        {
+            Console.WriteLine("kumar");
+        }
+        static void gradecalculator(int score)
+        {
+            Console.WriteLine(score);
+            manoh();
+        }
+        // entry point 
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            gradecalculator(12);
+           // manoh();
+        }
+    }
+}
