9151be80c1e4babf882901a7d8d8cfb90bd9291d
Narendra Yenugula <narendra.yenugula@mediamonks.com>
2020-11-19 07:06:16 +0100

    chanti program updated

--- CS PROJECTS/myapp/Program.cs
+++ CS PROJECTS/myapp/Program.cs
@@ -3,65 +3,72 @@ using System.Linq;
 
 namespace myapp
 {
-    class Program
+    class Practice
     {
-        
-    static void GradeCalculator(int score)
-    {
-        float percent = (score/1000f) * 100;
-
-        if(percent >= 90)
+        public void Start()
         {
-            Console.WriteLine( "outstanding performance");
-        }
-        else if (percent >= 70 && percent < 90)
-        {
-            Console.WriteLine( " GOOD ");
-        }
-        else if(percent >= 60 && percent < 70)
-        {
-            Console.WriteLine( " AVERAGE ");
+            logging("user0", "password0");
         }
 
-    }   
+
+        static void GradeCalculator(int score)
+        {
+            float percent = (score/1000f) * 100;
+
+            if(percent >= 90)
+            {
+                Console.WriteLine( "outstanding performance");
+            }
+            else if (percent >= 70 && percent < 90)
+            {
+                Console.WriteLine( " GOOD ");
+            }
+            else if(percent >= 60 && percent < 70)
+            {
+                Console.WriteLine( " AVERAGE ");
+            }
+
+        }   
     
-    static string [] userserver = new string[]{"user0", "user1", "user2"};
-    static string [] passwordserver = new string[] {"password0", "password1", "password2"};
+        string [] userserver = new string[]{"user0", "user1", "user2"};
+        string [] passwordserver = new string[] {"password0", "password1", "password2"};
 
-    static void logging ( string userid , string passwordid )
-    {
-    bool isuservalid =  userserver.Contains(userid);
-        Console.WriteLine(isuservalid);
-        if (    isuservalid )
+        void logging ( string userid , string passwordid )
         {
-            Console.WriteLine(" user available ");
-        }
-        else
-        {
-            Console.WriteLine(" user unavailable ");
-            return ;
-        }
+            bool isuservalid =  userserver.Contains(userid);
+            Console.WriteLine(isuservalid);
+            if (    isuservalid )
+            {
+                Console.WriteLine(" user available ");
+            }
+            else
+            {
+                Console.WriteLine(" user unavailable ");
+                return ;
+            }
 
-        int userindex = Array.IndexOf (userserver,userid);
-        int passwordindex = Array.IndexOf(passwordserver,passwordid);
+            int userindex = Array.IndexOf (userserver,userid);
+            int passwordindex = Array.IndexOf(passwordserver,passwordid);
 
-        if (userindex == passwordindex)
-        {
-            Console.WriteLine("login successsful ");
-        }
-        else 
-        {
-            Console.WriteLine(" login unsuccessful");
-        }
+            if (userindex == passwordindex)
+            {
+                Console.WriteLine("login successsful ");
+            }
+            else 
+            {
+                Console.WriteLine(" login unsuccessful");
+            }
 
+        }
     }
 
-    //logging("ncdjncjdnc","password1");   
+    class Program
+    {
         // entry point 
         static void Main()
         {
-            GradeCalculator(678);
-            logging("ncdjncjdnc","password1");   
+            var practice = new Practice();
+            practice.Start();
         }
     }
 }
