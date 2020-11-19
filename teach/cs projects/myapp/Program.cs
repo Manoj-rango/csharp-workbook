8cc0e0bbbd4ffe325963c27923b71a3723e3a71c
Narendra Yenugula <narendra.yenugula@mediamonks.com>
2020-11-19 06:54:35 +0100

    Update Program.cs

--- teach/cs projects/myapp/Program.cs
+++ teach/cs projects/myapp/Program.cs
@@ -1,41 +1,30 @@
 ﻿using System;
+using System.Linq;
 
 namespace myapp
 {
+    class Practice
+    {
+        public void Start()
+        {
+            Function1();
+        }
+
+        void Function1()
+        {
+            Console.WriteLine(" boo yeah");
+        }
+
+    }
+
     class Program
     {
-        static void Function2()
-        {
-            Console.WriteLine("Function2");
-        }
-
-        static void Function1()
-        {
-            Console.WriteLine("Function1");
-            Function2();
-        }
-
-        //give discount if balance is above 10,000
-        static float DiscountChecker(float balance, float price )
-        {
-            if(balance >= 10000)
-            {
-                float discountedPrice = (price * 0.90f);
-                Console.WriteLine($"Discounted price for {price} is {discountedPrice}");
-                return discountedPrice;
-            }
-            else
-            {
-                Console.WriteLine($"Your bank balance is less than 10,000");
-                return 0;
-            } 
-        }
-
 
         static void Main()
         {
             Console.WriteLine("Entry point");
-            DiscountChecker(50000, 200);
+            var practice = new Practice();
+            practice.Start();
         }
     }
 }
