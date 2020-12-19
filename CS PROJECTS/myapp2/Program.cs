e867c020774de5cb860e607c37250cc9d3096e37
Manoj-rango <manojk.yenugula@gmail.com>
2020-12-19 11:33:40 +0530

    after error correction

--- CS PROJECTS/myapp2/Program.cs
+++ CS PROJECTS/myapp2/Program.cs
@@ -9,8 +9,11 @@ namespace myapp2
             // var market = new DiscountPriceChecker();
             // market.CalculateDiscount1(100,"DIWALI50");
 
-            var billing = new Billing2();
-            billing.C_billing2();
+            // var billing = new Billing2();
+            // billing.C_billing2();
+
+            var AccountHolder = new Bank("Harry ",984873211);
+            AccountHolder.PrintDetails(); 
         }
     }
 }
